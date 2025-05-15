875da5423ceb87f695e109f834d8bb56386ee951
Thomas Cappelletti <thomas.cappelletti@incyclesoftware.com>
2025-05-15 10:56:31 -0400

    Created problem entity and model

---
+++ src/Microservice.WebApi/Entities/Problem.cs
@@ -0,0 +1,15 @@
+﻿using Swashbuckle.AspNetCore.Annotations;
+
+namespace Microservice.WebApi.Entities;
+
+public class Problem
+{
+    public string? Description { get; set; }
+
+    [SwaggerSchema(ReadOnly = true)]
+    public string? Id { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public string? Title { get; set; }
+}
---
+++ src/Microservice.WebApi/Models/ProblemModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Microservice.WebApi.Models;
+
+public class ProblemModel
+{
+    public string? Description { get; set; }
+
+    public string? Id { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public string? Title { get; set; }
+}
