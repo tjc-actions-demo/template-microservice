2880ded33c0e65c5792b95582df18b60e79d22a1
Thomas Cappelletti <thomas.cappelletti@incyclesoftware.com>
2025-05-15 11:30:45 -0400

    Added test data for Problems

---
+++ src/Microservice.UnitTests/Data/ProblemsTestData.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+using Microservice.WebApi.Entities;
+
+namespace Microservice.UnitTests.Data;
+
+[ExcludeFromCodeCoverage]
+public class ProblemsTestData : IEnumerable<object[]>
+{
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        yield return
+        [
+            new Problem
+            {
+                Description = "This is the description of Problem #1",
+                Id = "000000000000000000000001",
+                IsActive = true,
+                Title = "Problem #1"
+            },
+            ProblemDataIssues.None
+        ];
+
+        yield return
+        [
+            new Problem
+            {
+                Description = "This is the description of Problem #2",
+                Id = "000000000000000000000002",
+                IsActive = true,
+                Title = "Problem #2"
+            },
+            ProblemDataIssues.None
+        ];
+
+        yield return
+        [
+            new Problem
+            {
+                Description = "This is the description of Problem #3",
+                Id = "000000000000000000000003",
+                IsActive = true,
+                Title = "Problem #3"
+            },
+            ProblemDataIssues.None
+        ];
+
+        yield return
+        [
+            new Problem
+            {
+                Description = "This is the description of Problem #4",
+                Id = "000000000000000000000004",
+                IsActive = false,
+                Title = "Problem #4"
+            },
+            ProblemDataIssues.None
+        ];
+
+        yield return
+        [
+            new Problem
+            {
+                Id = "000000000000000000000005",
+                IsActive = false,
+                Title = "Problem #5"
+            },
+            ProblemDataIssues.MissingDescription
+        ];
+
+        yield return
+        [
+            new Problem
+            {
+                Description = "This is the description of Problem #6",
+                Id = "000000000000000000000006",
+                IsActive = false
+            },
+            ProblemDataIssues.MissingTitle
+        ];
+
+        yield return
+        [
+            new Problem
+            {
+                Id = "000000000000000000000007",
+                IsActive = false
+            },
+            ProblemDataIssues.MissingDescription | ProblemDataIssues.MissingTitle
+        ];
+    }
+}
+
+[Flags]
+public enum ProblemDataIssues
+{
+    None = 0,
+    MissingDescription = 1 << 0,
+    MissingTitle = 1 << 1
+}
