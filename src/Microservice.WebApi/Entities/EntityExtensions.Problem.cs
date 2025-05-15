c891ccc8b07dd5e36def4f6668feb1c180474a9b
Thomas Cappelletti <thomas.cappelletti@incyclesoftware.com>
2025-05-15 10:56:54 -0400

    Created extension methods to handel conversation between entity and model

---
+++ src/Microservice.WebApi/Entities/EntityExtensions.Problem.cs
@@ -0,0 +1,32 @@
+﻿using Microservice.WebApi.Models;
+
+namespace Microservice.WebApi.Entities;
+
+public static class EntityExtensions
+{
+    private static List<ProblemModel> ProblemsToProblemModels(IEnumerable<Problem> problems)
+    {
+        return problems.Select(problem => problem.ToModel()).ToList();
+    }
+
+    public static ProblemModel ToModel(this Problem problem)
+    {
+        return new ProblemModel
+        {
+            Description = problem.Description,
+            Id = problem.Id,
+            IsActive = problem.IsActive,
+            Title = problem.Title
+        };
+    }
+
+    public static List<ProblemModel> ToModels(this IList<Problem> problems)
+    {
+        return ProblemsToProblemModels(problems);
+    }
+
+    public static List<ProblemModel> ToModels(this IEnumerable<Problem> problems)
+    {
+        return ProblemsToProblemModels(problems);
+    }
+}
---
+++ src/Microservice.WebApi/Models/ModelExtensions.ProblemModel.cs
@@ -0,0 +1,18 @@
+﻿using Microservice.WebApi.Entities;
+
+namespace Microservice.WebApi.Models
+{
+    public static partial class ModelExtensions
+    {
+        public static Problem ToEntity(this ProblemModel problemModel)
+        {
+            return new Problem
+            {
+                Description = problemModel.Description,
+                Id = problemModel.Id,
+                IsActive = problemModel.IsActive,
+                Title = problemModel.Title
+            };
+        }
+    }
+}
