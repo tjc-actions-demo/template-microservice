f05c0d939f77dd72f2cf4d856adb9cd301010b7f
Thomas Cappelletti <thomas.cappelletti@incyclesoftware.com>
2025-05-15 10:57:07 -0400

    Created validator for ProblemModel

---
+++ src/Microservice.WebApi/Validators/ProblemModelValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Microservice.WebApi.Models;
+
+namespace Microservice.WebApi.Validators;
+
+public class ProblemModelValidator : AbstractValidator<ProblemModel>
+{
+    public ProblemModelValidator()
+    {
+        RuleFor(problem => problem.Description)
+            .NotEmpty()
+            .WithMessage("A problem must have a description.");
+
+        RuleFor(problem => problem.Title)
+            .NotEmpty()
+            .WithMessage("A problem must have title.");
+    }
+}
