2347b8d21b833a685148de6e066c610b5a85f5ed
Thomas Cappelletti <thomas.cappelletti@incyclesoftware.com>
2025-05-15 11:18:25 -0400

    Updated startup to use old style

---
+++ src/Microservice.WebApi/Startup.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using FluentValidation;
+using Microservice.WebApi.Models;
+using Microservice.WebApi.Services;
+using Microservice.WebApi.Validators;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Microservice.WebApi;
+
+[ExcludeFromCodeCoverage]
+public class Startup(IConfiguration configuration)
+{
+    public IConfiguration Configuration { get; } = configuration;
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseRouting();
+
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseAuthentication();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Add Services
+        services.AddSingleton<IProblemsService, ProblemsService>();
+
+        // Add Validators
+        services.AddScoped<IValidator<ProblemModel>, ProblemModelValidator>();
+
+        // Setup Controllers
+        services
+            .AddControllers()
+            .AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                options.SerializerSettings.Converters.Add(new StringEnumConverter());
+            });
+
+        // Add Swagger
+        services.AddSwaggerGen(options =>
+        {
+            var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+
+            options.SwaggerDoc("v1", new OpenApiInfo { Title = "Tsa.Submissions.Coding.WebApi", Version = "v1" });
+
+            options.EnableAnnotations();
+        });
+    }
+}
